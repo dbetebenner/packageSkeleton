86cc526dab97ac6829e6bb624ed6265adac721e6
Damian Betebenner <dbetebenner@nciea.org>
2024-12-03 16:25:20 -1000

    Updating startup message

--- R/zzz.R
+++ R/zzz.R
@@ -1,22 +1,20 @@
 .onLoad <- function(libname, pkgname) {
-  # You can initialize package settings or environment variables here if needed
 }
 
 .onAttach <- function(libname, pkgname) {
-  if (interactive()) {
-    # Extract version information
-    version <- packageVersion("packageSkeleton")
-    formatted_version <- gsub("\\.", "-", as.character(version))  # Replace dots with dashes
-    
-    # Define a friendly startup message
-    message_text <- paste0(
-      magenta$bold("packageSkeleton"), " v", formatted_version, 
-      " (11-4-2024)\n",
-      "For help, type: ", magenta$bold("> help(\"packageSkeleton\")"), " or visit:\n",
-      magenta$bold("https://centerforassessment.github.io/packageSkeleton")
-    )
-    
-    # Display the startup message
-    packageStartupMessage(message_text)
-  }
+    if (interactive()) {
+        # Extract version information
+        version <- utils::packageVersion("packageSkeleton")
+
+        # Define a friendly startup message
+	message_text <- paste0(
+	    magenta(bold("\uD83C\uDF89 packageSkeleton v", version)), " - ", toOrdinal::toOrdinalDate("2024-12-3"), "\n",
+	    "\U1F4A1 Tip: ", magenta(bold("> help(\"packageSkeleton\")")), "\n",
+	    "\U1F310 Docs: ", magenta(bold("https://centerforassessment.github.io/packageSkeleton")), "\n",
+	    "\u2728 Happy packageSkeletoning!"
+	)
+
+        # Display the startup message
+        packageStartupMessage(message_text)
+    }
 }
