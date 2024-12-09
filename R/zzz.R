e549bc949c6b53fb7eb63d747e14819744699a30
Damian Betebenner <dbetebenner@nciea.org>
2024-12-08 19:52:44 -0500

    Tidying up startup message

--- R/zzz.R
+++ R/zzz.R
@@ -5,37 +5,36 @@
     if (interactive()) {
 		# Utility function 
         get_dev_version <- function(package) {
-            url <- paste0("https://raw.githubusercontent.com/CenterForAssessment/", package, "/refs/heads/master/DESCRIPTION")
+            url <- paste0("https://raw.githubusercontent.com/CenterForAssessment/", package, "/refs/heads/main/DESCRIPTION")
             tryCatch({
                 lines <- readLines(url, warn = FALSE)
                 version_line <- grep("^Version:", lines, value = TRUE)
                 if (length(version_line) > 0) {
-                    return(strsplit(version_line, ": ")[[1]][2])
+                    return(cyan("v", strsplit(version_line, ": ")[[1]][2], sep=""))
                 } else {
-                    return("Unavailable")
+                    return(red("Not Available"))
                 }
             }, error = function(e) {
-                return("Unavailable")
+                return(red("Not Available"))
             }, warning = function(w) {
-                return("Unavailable")
+                return(red("Not Available"))
             })
         }
 
         # Extract version information
         installed.version <- utils::packageDescription("packageSkeleton")[['Version']]
         cran.version <- tryCatch(
-            pkgsearch::cran_package("packageSkeleton")[['Version']],
-            error = function(e) "Not Available",
-            warning = function(w) "Not Available")
+            green("v", pkgsearch::cran_package("packageSkeleton")[['Version']], sep=""),
+            error = function(e) red("Not Available"),
+            warning = function(w) red("Not Available"))
         dev.version <- get_dev_version("packageSkeleton")
 
         # Define a friendly startup message
 		message_text <- paste0(
 		    magenta(bold("\uD83C\uDF89 packageSkeleton v", installed.version, sep="")), " - ", toOrdinal::toOrdinalDate("2024-12-8"), "\n",
 			strrep("\u2501", 40), "\n",
-    	    bold("\U1F4E6 CRAN: "), red(cran.version), "\n",
-#    	    bold("\U1F4E6 CRAN: "), green("v", cran.version, sep=""), "\n",
-    	    bold("\U1F527 Dev: "), cyan("v", dev.version, sep=""), "\n",
+    	    bold("\U1F4E6 CRAN: "), cran.version, "\n",
+    	    bold("\U1F527 Dev: "), dev.version, "\n",
 			strrep("\u2501", 40), "\n",
 		    "\U1F4A1 Tip: ", magenta(bold("> help(package=\"packageSkeleton\")")), "\n",
 		    "\U1F310 Docs: ", magenta(bold("https://centerforassessment.github.io/packageSkeleton")), "\n",
